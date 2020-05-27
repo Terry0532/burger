f1669074b286acdf16f8309636bc9ac277bb2a20
Terry <t3rrygong@gmail.com>
2020-05-27 16:39:28 -0600

    it will print not devoured burgers with a button to devoure it. devoured burger will have a line through it.

--- controllers/burgers_controller.js
+++ controllers/burgers_controller.js
@@ -2,8 +2,12 @@ const burger = require("../models/burger");
 const express = require("express");
 const router = express.Router();
 
-router.get("/", function (req, res) {
-    res.render("index", { burger: burger.selectAll() })
+router.get("/", async function (req, res) {
+    let data;
+    await burger.selectAll().then(function (res) {
+        data = res;
+    });
+    res.render("index", { burger: data });
 })
 
 module.exports = router;
